f404031252eb51be6e7a9c270139ba69fc47b439
PriscoCoppola <priscocoppola7@gmail.com>
2021-04-20 14:37:44 +0200

    finish

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*****************************************
+* VUE 
+*****************************************/
+
+const root = new Vue({
+    el: '#root',
+    data: {
+        message: 'Stampare un\'immagine a schermo',
+        image: 'https://picsum.photos/200/300'
+    }
+})
